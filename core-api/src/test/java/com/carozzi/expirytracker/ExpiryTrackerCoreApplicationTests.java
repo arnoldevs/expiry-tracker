d1bada7589486be6c4195c9944337b460847b6f1
Arnold Vasquez <contacto@disroot.org>
2026-02-11 22:08:15 -0300

    chore: setup project baseline with docker config

---
+++ core-api/src/test/java/com/carozzi/expirytracker/ExpiryTrackerCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carozzi.expirytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpiryTrackerCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
